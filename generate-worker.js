22e51abd052d3ba53b6bf6ba8bca0fde6d561c3d
SamzxSong <159488383+SamzxSong@users.noreply.github.com>
2025-01-26 02:36:59 -0500

    add prod env

--- generate-worker.js
+++ generate-worker.js
@@ -173,7 +173,18 @@ const clickSingleOption = async (page, option = "") => {
 
 const generate = async () => {
   try {
-    const browser = await puppeteer.launch({ headless: false }); // Set headless: true for headless mode
+    const browser = await puppeteer.launch({
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
+    });
     const page = await browser.newPage();
 
     await page.goto("https://www.mybkexperience.com", {
